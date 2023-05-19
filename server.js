97a552064c8cc78eda3cd9c98d07924bfd4b1d19
khuptuang <90066103+khuptuang@users.noreply.github.com>
2023-05-18 20:08:51 -0400

    modify server.js

--- server.js
+++ server.js
@@ -41,12 +41,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder
-  app.use(express.static("frontend/build"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-}
